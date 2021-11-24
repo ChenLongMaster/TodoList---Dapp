84f6a3cd2be021c1d15c7568b6c180ede6ac9a06
ChenLongMaster <tranhoanglong51198@gmail.com>
2021-11-24 22:52:52 +0700

    merge unit test change

--- test/TodoList.test.js
+++ test/TodoList.test.js
@@ -21,20 +21,13 @@ contract('TodoList', (account) => {
         const taskCount = await this.todoList.taskCount();
         const task = await this.todoList.tasks(taskCount);
         assert.equal(task.id.toNumber(),taskCount.toNumber());
-<<<<<<< HEAD
         assert.equal(task.content, 'First Task');
         assert.equal(task.completed, false);
         assert.equal(taskCount, 1);
-=======
-        assert.equal(task.content, 'Second Task');
-        assert.equal(task.completed, false);
-        assert.equal(taskCount, 2);
->>>>>>> da9fc50f9079f26592335db253184f4d54561029
     })
 
     //insert test 
     it('create task', async () => {
-<<<<<<< HEAD
         const result = await this.todoList.createTask('Second Task');
         const taskCount = await this.todoList.taskCount();
         const event = result.logs[0].args;
@@ -42,14 +35,5 @@ contract('TodoList', (account) => {
         assert.equal(event.content, 'Second Task');
         assert.equal(event.completed, false);
         assert.equal(taskCount , 2);
-=======
-        const result = await this.todoList.createTask('Third Task');
-        const taskCount = await this.todoList.taskCount();
-        const event = result.logs[0].args;
-        assert.equal(event.id.toNumber(),taskCount.toNumber());
-        assert.equal(event.content, 'Third Task');
-        assert.equal(event.completed, false);
-        assert.equal(taskCount , 3);
->>>>>>> da9fc50f9079f26592335db253184f4d54561029
     });
 });
